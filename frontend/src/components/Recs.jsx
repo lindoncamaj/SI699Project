ae0c271c0d9bead67c100865d48a2c607726e2b5
Rio Capollari <rcapollari@oakland.edu>
2025-02-24 19:28:52 -0500

    creates recommendations file

---
+++ frontend/src/components/Recs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Recs = () => {
+    return (
+        <div>
+            <h1>Recommendations</h1>
+            {/* Add your component content here */}
+        </div>
+    );
+};
+
+export default Recs;
