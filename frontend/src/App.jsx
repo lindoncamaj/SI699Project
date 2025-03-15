94ffab9dbfc8497808db540777cdccd6a20b469f
lindoncamaj <143720623+lindoncamaj@users.noreply.github.com>
2025-03-15 12:09:38 -0400

    Add frontend files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Form from "./components/Form";
 import Recs from "./components/Recs";
+import Listings from "./components/Listings";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Form />} />
             <Route path="/recs" element={<Recs />} />
+            <Route path="/listings" element={<Listings />} />
           </Routes>
         </Router>
   );
