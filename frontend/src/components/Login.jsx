f1e8e4a55f808d32825310160aa5559d73208738
lindoncamaj <143720623+lindoncamaj@users.noreply.github.com>
2025-03-31 10:40:58 -0400

    Add Login.jsx

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,93 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
+
+function Login() {
+  const [user_name, setUsername] = useState("");
+  const [user_pass, setPassword] = useState("");
+
+  const navigate = useNavigate(); // Initialize navigation
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user_name, user_pass);
+
+    // Validation check: Ensure all fields are filled
+    if (!user_name || !user_pass) {
+      alert("Please fill out all fields before submitting.");
+      return;
+    }
+
+    const data = {
+      user_name,
+      user_pass
+    };
+
+    // Pass form data to flask login function
+    axios.post("http://localhost:8080/login", data, { withCredentials: true }).then((response) => {
+      alert(response.data.message);
+      if (response.data.message === "Successfully Logged-in") {
+        navigate("/");
+      } else {
+        navigate("/login");
+      }
+    });
+  };
+  const handleReset = () => {
+    // Reset all state variables here
+    setUsername("");
+    setPassword("");
+  };
+
+  const handleRegisterClick = () => {
+    navigate("/register");
+  };
+  return (
+    <div className="Form">
+      <h1>Login</h1>
+      <a href="#" onClick={handleRegisterClick}>Register</a>
+      <fieldset>
+        <form action="#" method="get">
+          <label htmlFor="user_name">Username</label>
+          <input
+            type="string"
+            name="user_name"
+            id="user_name"
+            value={user_name}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter username"
+            required
+          />
+          <label htmlFor="user_pass">Password</label>
+          <input
+            type="password"
+            name="user_pass"
+            id="user_pass"
+            value={user_pass}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="*******"
+            required
+          />
+          <button
+            id="reset"
+            type="reset"
+            value="reset"
+            onClick={() => handleReset()}
+          >
+            Reset
+          </button>
+          <button
+            id="submit"
+            type="submit"
+            value="Submit"
+            onClick={(e) => handleSubmit(e)}
+          >
+            Submit
+          </button>
+        </form>
+      </fieldset>
+    </div>
+  );
+}
+
+export default Login;
