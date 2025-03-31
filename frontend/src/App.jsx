fc24d85568022556ea83065b749e6e8ee536903d
lindoncamaj <143720623+lindoncamaj@users.noreply.github.com>
2025-03-31 10:40:22 -0400

    Include login and register pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import Form from "./components/Form";
 import Recs from "./components/Recs";
 import Listings from "./components/Listings";
+import Login from "./components/Login";
+import Register from "./components/Register";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
             <Route path="/" element={<Form />} />
             <Route path="/recs" element={<Recs />} />
             <Route path="/listings" element={<Listings />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </Router>
   );
